8700b15dd383a5cc8a001c9145ec649d4ae047d6
leonardoFerreira096 <leonardo_costaferreira22@hotmail.com>
2025-10-28 01:05:23 -0300

    <01738044 - Leonardo - Fucionaliade: config do firebase p2>

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,16 +1,18 @@
+// firebaseConfig.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-// import { db, auth } from "./firebaseConfig"
+// import { db, auth } from "./firebaseConfig" 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDA3xZprmU0ZbAYudv5kTx6tJuUKWhiu3U",
-  authDomain: "db-app-descubrafortaleza.firebaseapp.com",
-  projectId: "db-app-descubrafortaleza",
-  storageBucket: "db-app-descubrafortaleza.firebasestorage.app",
-  messagingSenderId: "756999984807",
-  appId: "1:756999984807:web:0f821b0111c8471f3dad82"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
